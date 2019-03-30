b8397359a7cfe7343997694a0e9957d0ec77e905
Edward Yan <eyanje@gmail.com>
2019-03-30 18:09:40 -0400

    Added Save

---
+++ src/main/java/com/glowingpigeon/pigeonbound/data/Save.java
@@ -0,0 +1,18 @@
+
+public class Save {
+    /**
+     * Constructs a new, empty save
+     */
+    public Save() {
+
+    }
+
+    /**
+     * Loads a save from path
+     * 
+     * @param path the path to the save file
+     */
+    public Save(String path) {
+
+    }
+}
