ba3b00563af5075006351e778be90d009414a26d
Edward Yan <eyanje@gmail.com>
2019-03-31 01:56:22 -0400

    Clear PlayState

--- src/main/java/com/glowingpigeon/pigeonbound/state/PlayState.java
+++ src/main/java/com/glowingpigeon/pigeonbound/state/PlayState.java
@@ -2,6 +2,8 @@ package com.glowingpigeon.pigeonbound.state;
 
 import com.glowingpigeon.pigeonbound.data.*;
 
+import javafx.scene.canvas.GraphicsContext;
+
 public class PlayState extends GameState {
     Save save;
     
@@ -11,5 +13,10 @@ public class PlayState extends GameState {
 
     public PlayState(String savePath) {
         save = new Save(savePath);
+    }
+
+    @Override
+    public void render(GraphicsContext gc) {
+        gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
     }
 }
